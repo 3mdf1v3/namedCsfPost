6798a422f1d7d65d4dbb8a31adff904d9ec5b2c7
3mdf1v3 <3mdf1v3@gmail.com>
2018-12-31 14:52:29 +0100

    Added invalid request

--- namedCsfPost.sh
+++ namedCsfPost.sh
@@ -12,15 +12,20 @@ iptables -I INPUT -p udp -m string --hex-string "|000000010000ff0001000029232800
 iptables -A DNSHEXINDROP -j LOG --log-prefix="*** DNSHEXINDROP DROPPED *** "
 iptables -A DNSHEXINDROP -j DROP
 
+iptables -N DNSINVALIDREC
+iptables -I INPUT -p udp --dport 53 -m string --algo kmp --from 30 --hex-string "|01000001000000000000|" -j DNSINVALIDREC 
+# iptables -A DNSINVALIDREC -j LOG --log-prefix="*** DNSINVALIDREC DROPPED *** "
+iptables -A DNSINVALIDREC -j DROP
+
 iptables -N DNS8105DROP
-iptables -I INPUT -p udp --source-port 53 -m string --algo kmp --from 30 --to 31 --hex-string "|8105|" -j DNS8105DROP
+iptables -I INPUT -p udp --dport 53 -m string --algo kmp --from 30 --to 31 --hex-string "|8105|" -j DNS8105DROP
 iptables -A DNS8105DROP -j LOG --log-prefix="*** DNS8105DROP DROPPED *** "
 iptables -A DNS8105DROP -j DROP
 
 iptables -N DNSANYQUERY
 iptables -I INPUT -p udp --dport 53 -m string --algo bm --hex-string '|0000FF0001|' -m recent --set --name dnsanyquery
-iptables -I INPUT -p udp --dport 53 -m string --algo bm --hex-string "|0000FF0001|" -m recent --name dnsanyquery --rcheck --seconds 10 --hitcount 3 -j DNSANYQUERY
-iptables -A DNSANYQUERY -j LOG --log-prefix="*** DNSANYQUERY DROPPED *** "
+iptables -I INPUT -p udp --dport 53 -m string --algo bm --hex-string "|0000FF0001|" -m recent --name dnsanyquery --rcheck --seconds 10 --hitcount 5 -j DNSANYQUERY
+#iptables -A DNSANYQUERY -j LOG --log-prefix="*** DNSANYQUERY DROPPED *** "
 iptables -A DNSANYQUERY -j DROP
 
 
@@ -31,13 +36,18 @@ ip6tables -I INPUT -p udp -m string --hex-string "|000000010000ff000100002923280
 ip6tables -A DNSHEXINDROP -j LOG --log-prefix="*** DNSHEXINDROP DROPPED *** "
 ip6tables -A DNSHEXINDROP -j DROP
 
+ip6tables -N DNSINVALIDREC
+ip6tables -I INPUT -p udp --dport 53 -m string --algo kmp --from 30 --hex-string "|01000001000000000000|" -j DNSINVALIDREC
+# ip6tables -A DNSINVALIDREC -j LOG --log-prefix="*** DNSINVALIDREC DROPPED *** "
+ip6tables -A DNSINVALIDREC -j DROP
+
 ip6tables -N DNS8105DROP
-ip6tables -I INPUT -p udp --source-port 53 -m string --algo kmp --from 30 --to 31 --hex-string "|8105|" -j DNS8105DROP
+ip6tables -I INPUT -p udp --dport 53 -m string --algo kmp --from 30 --to 31 --hex-string "|8105|" -j DNS8105DROP
 ip6tables -A DNS8105DROP -j LOG --log-prefix="*** DNS8105DROP DROPPED *** "
 ip6tables -A DNS8105DROP -j DROP
 
 ip6tables -N DNSANYQUERY
 ip6tables -I INPUT -p udp --dport 53 -m string --algo bm --hex-string '|0000FF0001|' -m recent --set --name dnsanyquery
 ip6tables -I INPUT -p udp --dport 53 -m string --algo bm --hex-string "|0000FF0001|" -m recent --name dnsanyquery --rcheck --seconds 10 --hitcount 3 -j DNSANYQUERY
-ip6tables -A DNSANYQUERY -j LOG --log-prefix="*** DNSANYQUERY DROPPED *** "
+#ip6tables -A DNSANYQUERY -j LOG --log-prefix="*** DNSANYQUERY DROPPED *** "
 ip6tables -A DNSANYQUERY -j DROP
